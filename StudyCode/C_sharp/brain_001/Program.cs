4d8febe2cb0df6465edeaede244ced0af4eb6089
coox <coox@neowiz.com>
2019-07-30 13:18:13 +0900

    [Exercise/C#] foreach

---
+++ StudyCode/C_sharp/brain_001/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForEachTest
+{
+    class MainApp
+    {
+        static void Main (String[] args)
+        {
+            int[] arr = new int[] { 0, 1, 2, 3, 4 };
+
+            foreach ( int a in arr )
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
