d4aad372d859ea43cc217ea60e9182639be413a5
jonghocook <jonghocook@gmail.com>
2020-06-28 00:29:50 +0900

    [c]exercise

---
+++ c/20200623/02_str.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char* str1 = "Good!";
+    char* str2 = "Good!";
+    printf("%d | %d \n", str1, str2);
+    
+    return 0;
+}
---
+++ c/20200624/02_arr_fct.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void fct(int*);
+
+int main(void)
+{
+    int arr1[2] = {1, 2};
+
+    fct(arr1);
+    printf("%d \n", arr1[0]);
+
+    return 0;
+}
+
+void fct(int* arr2)
+{
+    printf("%d \n", arr2[0]);
+    arr2[0] = 3;
+    return;
+}
