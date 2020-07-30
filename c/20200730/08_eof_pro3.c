59370d1227048c7ee71c9ede452fac3f4c5c47c7
jonghocook <jonghocook@gmail.com>
2020-07-30 18:39:16 +0900

    [c]Practice

---
+++ c/20200730/08_eof_pro3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int state;
+    char ch;
+
+    FILE* file = fopen("test.txt", "wb");
+    if(file==NULL)
+    {
+        printf("File open error!\n");
+        return 1;
+    }
+
+    fputc('1', file);
+    fputc((char)255, file);
+    fputc('2', file);
+
+    state = fclose(file);
+    if(state!=0)
+    {
+        printf("File close error!\n");
+        return 1;
+    }
+
+    file = fopen("test.txt", "rb");
+    if(file==NULL)
+    {
+        printf("File open error!\n");
+        return 1;
+    }
+
+    while(1)
+    {
+        ch = fgetc(file);
+        if(feof(file)!=0)
+        {
+            break;
+        }
+        printf("Data : %c \n", ch);
+    }
+
+    state = fclose(file);
+    if(state!=0)
+    {
+        printf("File close error!\n");
+        return 1;
+    }
+    
+    return 0;
+}
