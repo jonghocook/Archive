c6787f67bd88a100a40c6908b11676e3754cc905
jonghocook <jonghocook@gmail.com>
2020-08-11 01:55:00 +0900

    [csharp]Exercise

---
+++ csharp/20200811/02_overflow/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Overflow
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = int.MaxValue;
+            Console.WriteLine(a);
+
+            a += 1;
+
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ csharp/20200811/03_Decimal_Char/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Char
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            charExercise exercise = new charExercise();
+            string d = _rest();
+
+            float a = 3.1425_9265_3589_7932_3846_2643_3832_79f;
+            double b = 3.1425_9265_3589_7932_3846_2643_3832_79;
+            decimal c = 3.1425_9265_3589_7932_3846_2643_3832_79m;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+            exercise.printString();
+            exercise.printBoolType();
+        }
+
+        static string _rest()
+        {
+            string a = "안녕하세요?";
+            string b = "국종호입니다!\n";
+
+            return a + b;
+            
+        }
+    }
+
+    class charExercise
+    {
+        public void printString()
+        {
+            char a = '안';
+            char b = '녕';
+            char c = '하';
+            char d = '세';
+            char e = '요';
+
+            Console.Write(a);
+            Console.Write(b);
+            Console.Write(c);
+            Console.Write(d);
+            Console.Write(e);
+            Console.WriteLine();
+        }
+
+        public void printBoolType()
+        {
+            bool a = true;
+            bool b = false;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
